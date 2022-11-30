b970c0fccbcee1ee46ac3b5483fad5a612ada371
fidanda <109985560+fidanda@users.noreply.github.com>
2022-11-30 18:22:28 -0500

    Delete pet_desk directory
    no need anymore

--- pet_desk/src/app/appointment.ts
+++
@@ -1,8 +0,0 @@
-export interface Appointment 
-{
-    id: number,
-    pet_id: number,
-    appt_date: string,
-    provider: string,
-    appt_notes: string
-}
--- pet_desk/src/app/rx.ts
+++
@@ -1,9 +0,0 @@
-export interface Rx 
-{
-    id: number,
-    pet_id: number,
-    appt_id: number,
-    rx_name: string,
-    rx_reminder: string,
-    rx_notes: string
-}
